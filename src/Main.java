d73cb50072b4715358728a98ed1f7915bf85987b
Дмитрий <dimvik007@gmail.com>
2026-05-25 20:01:05 +1000

    zadanie все исправил

--- src/Main.java
+++ src/Main.java
@@ -137,19 +137,18 @@ public class Main {
         System.out.println(g);
 
 
-
         byte xl = 67;
-        System.out.println("Значение переменой xl с типом byte равно " + xl);
         int xa = 27897;
-        System.out.println("Значение с переменой xa с типом int равно " + xa);
-        short xb =569;
-        System.out.println("Значение с переменой xb с типом short равно " + xb);
-        long xc = 987678965;
-        System.out.println("Значение с переменой xc с типом long равно " + xc);
+        short xb = 569;
+        long xc = 987678965L;
         float xv = 27.12f;
-        System.out.println("Значение с переменой xv с типос float равно " + xv);
-        double xz =2.786;
+        double xz = 2.786;
         System.out.println("Значение с переменой xz с типом double равно " + xz);
+        System.out.println("Значение переменой xl с типом byte равно " + xl);
+        System.out.println("Значение с переменой xa с типом int равно " + xa);
+        System.out.println("Значение с переменой xb с типом short равно " + xb);
+        System.out.println("Значение с переменой xc с типом long равно " + xc);
+        System.out.println("Значение с переменой xv с типом float равно " + xv);
 
 
         byte LP = 23;
@@ -161,63 +160,63 @@ public class Main {
         int vseList = 480;
         byte vseUcheniki = (byte) (LP + AS + EA);
         System.out.println(vseUcheniki + " учеников учаться в трех классах");
-        int listKajdomu =  (vseList / vseUcheniki);
+        int listKajdomu = (vseList / vseUcheniki);
         System.out.println("На каждого ученика рассчитано " + listKajdomu + " листов бумаги");
 
 
         byte twoMin = 16;
         byte sa = 2;
-        byte sq= 1;
+        byte sq = 1;
         byte oneMin = (byte) (twoMin / sa);
-        System.out.println("За "+ sq + " минуту производит "+ oneMin + " штук бутылок!" );
-        System.out.println("за "+ sa + " минуты произвела " + twoMin + " штук бутылок!");
+        System.out.println("За " + sq + " минуту производит " + oneMin + " штук бутылок!");
+        System.out.println("за " + sa + " минуты произвела " + twoMin + " штук бутылок!");
         byte sv = 20;
         int sd = sv / sa;
         int se = twoMin * sd;
-        System.out.println("За "+ sv + " минут произвела " + se + " штук бутылок!" );
+        System.out.println("За " + sv + " минут произвела " + se + " штук бутылок!");
         byte sx = 24;
         int minSx = sx * 60;
         int sr = minSx * oneMin;
         System.out.println("в 1х сутках " + sx + " часа!");
         System.out.println("В одних сутках " + minSx + " минут!");
-        System.out.println("За "+ minSx + " минут произвела " + sr + " штук бутылок!");
+        System.out.println("За " + minSx + " минут произвела " + sr + " штук бутылок!");
         byte su = 3 * 24;
-        int  sf = su * 60;
+        int sf = su * 60;
         int st = sf * oneMin;
         System.out.println("В трех сутках " + su + " часа");
-        System.out.println("В трех сутках "+ sf + " минут");
-        System.out.println("За "+ sf + " минут произвела "+ st + " штук бутылок!" );
+        System.out.println("В трех сутках " + sf + " минут");
+        System.out.println("За " + sf + " минут произвела " + st + " штук бутылок!");
         byte so = 30;
         int sh = so * 24;
         int sk = sh * 60;
         int sl = sk * oneMin;
-        System.out.println("В одном месяце "+ sh + " часов");
-        System.out.println("В одном месяце "+ sk + " минут");
-        System.out.println("За "+ sk + " минут произвела "+ sl + " штук бутылок!");
+        System.out.println("В одном месяце " + sh + " часов");
+        System.out.println("В одном месяце " + sk + " минут");
+        System.out.println("За " + sk + " минут произвела " + sl + " штук бутылок!");
 
 
-         byte banok = 120;
-         byte oneBanka = 2;
-         byte twoBanka = 4;
-         byte vseBanka = (byte) (oneBanka + twoBanka);
-         byte vseClass = (byte) (banok / vseBanka);
-         byte korKras = (byte) (twoBanka * vseClass);
-         byte belKras= (byte) (oneBanka * vseClass);
-        System.out.println("Нужно для одного класса " + vseBanka + " банок краски разного цвета");
-         System.out.println("Всего "+ vseClass + " классов в школе!");
-        System.out.println("В школе, где "+ vseClass + " классов, нужно "+ belKras + " банок белой краски и " + korKras + " банок коричневой краски! ");
+        byte cans = 120;
+        byte oneCans = 2;
+        byte twoCans = 4;
+        byte allCans = (byte) (oneCans + twoCans);
+        byte allClasses = (byte) (cans / allCans);
+        byte brownPaint = (byte) (twoCans * allClasses);
+        byte whitePaint = (byte) (oneCans * allClasses);
+        System.out.println("Нужно для одного класса " + allCans + " банок краски разного цвета");
+        System.out.println("Всего " + allClasses + " классов в школе!");
+        System.out.println("В школе, где " + allClasses + " классов, нужно " + whitePaint + " банок белой краски и " + brownPaint + " банок коричневой краски! ");
 
 
-        var banan = 5;
+        byte banan = 5;
         byte bana = 80;
-        var milk = 200;
+        int milk = 200;
         byte milc = 105;
-        var ml = milk / 100;
+        byte ml = (byte) (milk / 100);
         System.out.println(ml + "  пачrb молока по 100мл в нашей корзине");
-        System.out.println("В 100 мл молока "+ milc + " граммов" );
-        var iceCrem = 2;
+        System.out.println("В 100 мл молока " + milc + " граммов");
+        byte iceCrem = 2;
         byte cremIce = 100;
-        var egg = 4;
+        byte egg = 4;
         byte eg = 70;
         int Banan = banan * bana;
         System.out.println("Всего " + Banan + " граммов банана");
@@ -226,77 +225,75 @@ public class Main {
         int ice = iceCrem * cremIce;
         System.out.println("Всего " + ice + " граммов мороженного");
         int Egg = egg * eg;
-        System.out.println("Всего "+ Egg + " граммов яиц");
+        System.out.println("Всего " + Egg + " граммов яиц");
         int vseProdukt = Banan + Milk + ice + Egg;
-        var grm = 1000;
-        var kg = 1;
-        System.out.println("В "+ kg + " килограмме "+ grm + " граммов");
+        int grm = 1000;
+        byte kg = 1;
+        System.out.println("В " + kg + " килограмме " + grm + " граммов");
         float vseKillo = (float) vseProdukt / grm;
         System.out.println("Вес завтрака состовляет " + vseProdukt + " граммов!");
         System.out.println("Вес завтрака состовляет " + vseKillo + " килограммов!");
 
 
-        var ves = 7;
-        var vesUbrat = ves * 1000;
-        var pohudenieOne = 250;
-        var pohudenieTwo = 500;
-        var skolDay = vesUbrat / pohudenieOne;
-        var skolkoDay = vesUbrat / pohudenieTwo;
-        System.out.println( vesUbrat + " граммов. Столько надо сбросить спорцмену до соревнований!");
-        System.out.println(skolDay + " дней понядобиться на сгон веса, если сгонять по 250 грамм ");
-        System.out.println(skolkoDay + " дней понадобиться на сгон веса, если сгонять по 500 грамм ");
-
+        byte weight = 7;
+        short removeWeight = (short) (weight * 1000);
+        short weighLoss = 250;
+        short loseWeight = 500;
+        short skipDays = (short) (removeWeight / weighLoss);
+        short SkipDay = (short) (removeWeight / loseWeight);
+        System.out.println(removeWeight + " граммов. Столько надо сбросить спорцмену до соревнований!");
+        System.out.println(skipDays + " дней понядобиться на сгон веса, если сгонять по 250 грамм ");
+        System.out.println(SkipDay + " дней понадобиться на сгон веса, если сгонять по 500 грамм ");
 
 
         int masha = 67760;
         int denis = 83690;
         int kristina = 76230;
 
-        float procentZpMasha = (float) masha/ 100 ;
+        float procentZpMasha = (float) masha / 100;
         float zpMashaProcent = (float) procentZpMasha * 10;
-        System.out.println("На "+ zpMashaProcent + " рублей повыситься зарплата у Маши ");
+        System.out.println("На " + zpMashaProcent + " рублей повыситься зарплата у Маши ");
 
-        float procentZpDenis = (float) denis/ 100 ;
+        float procentZpDenis = (float) denis / 100;
         float zpDenisProcent = (float) procentZpDenis * 10;
-        System.out.println("На "+ zpDenisProcent + " рублей повыситься зарплата у Дениса ");
+        System.out.println("На " + zpDenisProcent + " рублей повыситься зарплата у Дениса ");
 
-        float procentZpKristina = (float) kristina/ 100 ;
+        float procentZpKristina = (float) kristina / 100;
         float zpKristinaProcent = (float) procentZpKristina * 10;
-        System.out.println("На "+ zpKristinaProcent + " рублей повыситься зарплата у Кристины ");
+        System.out.println("На " + zpKristinaProcent + " рублей повыситься зарплата у Кристины ");
 
         int zpMasha = (int) (masha + zpMashaProcent);
         int zpDenis = (int) (denis + zpDenisProcent);
         int zpKristina = (int) (kristina + zpKristinaProcent);
-        System.out.println("Зарплата Маши с прибавление составляет "+ zpMasha);
-        System.out.println("Зарплата Дениса с прибавление составляет "+ zpDenis);
-        System.out.println("Зарплата Кристины с прибавление составляет "+ zpKristina);
+        System.out.println("Зарплата Маши с прибавление составляет " + zpMasha);
+        System.out.println("Зарплата Дениса с прибавление составляет " + zpDenis);
+        System.out.println("Зарплата Кристины с прибавление составляет " + zpKristina);
 
         int ninZpMasha = masha * 12;
         int ninZpDenis = denis * 12;
         int ninZpKristina = kristina * 12;
-        System.out.println("Нынешняя зарплата составляет Маши "+ ninZpMasha + " рублей в год!");
-        System.out.println("Нынешняя зарплата составляет Дениса "+ ninZpDenis + " рублей в год!");
-        System.out.println("Нынешняя зарплата составляет Кристины "+ ninZpKristina + " рублей в год!");
+        System.out.println("Нынешняя зарплата составляет Маши " + ninZpMasha + " рублей в год!");
+        System.out.println("Нынешняя зарплата составляет Дениса " + ninZpDenis + " рублей в год!");
+        System.out.println("Нынешняя зарплата составляет Кристины " + ninZpKristina + " рублей в год!");
 
         int seyZpMasha = zpMasha * 12;
         int seyZpDenis = zpDenis * 12;
         int seyZpKristina = zpKristina * 12;
-        System.out.println("Зарплата Маши с прибавлением процентов составляет "+ seyZpMasha+ " рублей в год!");
-        System.out.println("Зарплата Дениса с прибавлением процентов составляет "+ seyZpDenis+ " рублей в год");
-        System.out.println("Зарплата Кристины с прибавлением процентов составляет "+ seyZpKristina+ " рублей в год");
+        System.out.println("Зарплата Маши с прибавлением процентов составляет " + seyZpMasha + " рублей в год!");
+        System.out.println("Зарплата Дениса с прибавлением процентов составляет " + seyZpDenis + " рублей в год");
+        System.out.println("Зарплата Кристины с прибавлением процентов составляет " + seyZpKristina + " рублей в год");
 
         int razZpMasha = seyZpMasha - ninZpMasha;
         int razZpDenis = seyZpDenis - ninZpDenis;
         int razZpKristina = seyZpKristina - ninZpKristina;
-        System.out.println("Машина разница между нынешней зп и с прибавкой составляет "+ razZpMasha+ " рублей за год!");
-        System.out.println("Дениса разница между нынешней зп и с прибавкой составляет "+ razZpDenis+ " рублей за год!");
-        System.out.println("Кристинина разница между нынешней зп и с прибавкой составляет "+ razZpKristina+ " рублей за год!");
+        System.out.println("Машина разница между нынешней зп и с прибавкой составляет " + razZpMasha + " рублей за год!");
+        System.out.println("Дениса разница между нынешней зп и с прибавкой составляет " + razZpDenis + " рублей за год!");
+        System.out.println("Кристинина разница между нынешней зп и с прибавкой составляет " + razZpKristina + " рублей за год!");
 
-        System.out.println("Маша теперь получает "+ seyZpMasha + " рублей. Годовой доход вырос на " + razZpMasha + " рублей");
-        System.out.println("Денис теперь получает "+ seyZpDenis + " рублей. Годовой доход вырос на " + razZpDenis + " рублей");
-        System.out.println("Кристина теперь получает "+ seyZpKristina + " рублей. Годовой доход вырос на " + razZpKristina + " рублей");
+        System.out.println("Маша теперь получает " + seyZpMasha + " рублей. Годовой доход вырос на " + razZpMasha + " рублей");
+        System.out.println("Денис теперь получает " + seyZpDenis + " рублей. Годовой доход вырос на " + razZpDenis + " рублей");
+        System.out.println("Кристина теперь получает " + seyZpKristina + " рублей. Годовой доход вырос на " + razZpKristina + " рублей");
 
 
-
-         }
     }
+}
